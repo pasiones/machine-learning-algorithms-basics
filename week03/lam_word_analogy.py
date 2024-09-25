a745fec30fd8378595222e1bd3e49bbeaa3f24e0
Dat Minh Lam <datminh.lam@tuni.fi>
2024-09-25 12:50:29 +0300

    results include distances

--- week03/lam_word_analogy.py
+++ week03/lam_word_analogy.py
@@ -20,12 +20,7 @@ X = np.array([vectors[word] for word in words])
 def euclidean_distance(vec1, vec2):
     return np.linalg.norm(np.array(vec1) - np.array(vec2))
 
-def find_analogy(input_term_0, input_term_1, input_term_2, X, words, vectors, k = 2):
-    if (input_term_0 not in vectors 
-        or input_term_1 not in vectors 
-        or input_term_2 not in vectors):
-        return []
-    
+def find_analogy(input_term_0, input_term_1, input_term_2, X, words, vectors, k):   
     vec_0 = vectors[input_term_0]
     vec_1 = vectors[input_term_1]
     vec_2 = vectors[input_term_2]
@@ -40,25 +35,29 @@ def find_analogy(input_term_0, input_term_1, input_term_2, X, words, vectors, k 
 
     results = [words[i] for i in indices 
                if words[i] not in {input_term_0, input_term_1, input_term_2}][:k]
-
-    return results
+    returned_distances = [distances[i] for i in indices
+                        if words[i] not in {input_term_0, input_term_1, input_term_2}][:k]
+    return results, returned_distances
 
 
 def main():
+    no_of_return_values = 4
     while True:
-        input_term = input("\nEnter three words (or 'quit' to exit): ").split()
+        input_term = input("\nEnter three words separated by space (or 'quit' to exit): ").split()
 
         if input_term[0] == 'quit':
             break
         
-        if (input_term[0] in vectors 
-            and input_term[1] in vectors 
-            and input_term[2] in vectors):
-            results = find_analogy(input_term[0], input_term[1], input_term[2],
-                                    X, words, vectors)
-            print(f"{input_term[0]} is to {input_term[1]} as {input_term[2]} is to: ")
-            for i, word in enumerate(results, 1):
-                print(f"{i}: {word}")
+        elif (input_term[0] not in vectors 
+            and input_term[1] not in vectors 
+            and input_term[2] not in vectors):
+            print("Word not found")
+            continue
 
+        results = find_analogy(input_term[0], input_term[1], input_term[2],
+                                    X, words, vectors, no_of_return_values)
+        print(f"{input_term[0]} is to {input_term[1]} as {input_term[2]} is to: ")
+        for i in range(no_of_return_values):
+            print(f"{i+1}: {results[0][i]} ({results[1][i]})")
 if __name__ == "__main__":
     main()
